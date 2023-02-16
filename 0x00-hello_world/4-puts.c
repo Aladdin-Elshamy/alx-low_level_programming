fea006c2505d7277c2e19ed3909acc835c088e4c
Aladdin-Elshamy <aladdinelshamy@gmail.com>
2023-02-16 09:00:36 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 /* 
- * the main function 
- * prints a line with puts function
- * return 0 when success
- * */
+ the main function 
+ prints a line with puts function
+ return 0 when success
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
