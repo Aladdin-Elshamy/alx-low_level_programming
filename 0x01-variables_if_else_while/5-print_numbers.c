34cb40fe84a31c4ad47ce8e7dacaf1af475016c5
Aladdin-Elshamy <aladdinelshamy@gmail.com>
2023-02-17 05:54:27 -0800

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the numbers of base 10 starting form 0 to 9
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	for (int i = 0; i <= 9; i++)
+		printf("%d", i);
+	return (0);
+}
