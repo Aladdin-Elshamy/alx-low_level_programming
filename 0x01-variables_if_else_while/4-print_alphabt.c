ab92c2a6931ae610d0b52b892f2aa996db210ef5
Aladdin-Elshamy <aladdinelshamy@gmail.com>
2023-02-17 05:28:36 -0800

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print all the letters except q and e
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n != 101 && n != 113)
+		{
+			continue;
+		}
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
