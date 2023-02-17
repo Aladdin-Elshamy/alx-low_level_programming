31a4c9ec35d72ef0026a8e3d927891a249fa8ae0
Aladdin-Elshamy <aladdinelshamy@gmail.com>
2023-02-17 09:40:26 -0800

    9

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints number in base 16 followed by a new line
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < = 9; i++)
+	{
+		putchar(i + 48);
+
+		if (i == 9)
+		{
+			int j;
+
+			for (j = 97; j <= 102; j++)
+				putchar(j);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
